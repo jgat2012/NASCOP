b7f10e3b71f611031750a3ea7c28aa07cddcb2d2
KevinMarete55443 <kevomarete@gmail.com>
2013-09-13 15:18:23 +0300

    TableTools

--- application/views/dashboard/new_v.php
+++ application/views/dashboard/new_v.php
@@ -1,35 +1,13 @@
 <?php $this -> load -> view('sections/head');?>
 <script type="text/javascript">
 	$(document).ready(function() {
-		/*
-		 var oTable = $('#patient_listing').dataTable({
-		 "sScrollX" : "100%",
-		 "sScrollY" : "300px",
-		 "sScrollXInner" : "",
-		 "bScrollCollapse" : true,
-		 "bPaginate" : true,
-		 "bJQueryUI" : true,
-		 "bAutoWidth": false,
-		 });
-		 new FixedColumns(oTable);
-
-		 var oTable = $('#patient_listing').dataTable({
-		 "sScrollX" : "100%",
-		 "sScrollY" : "300px",
-		 "sScrollXInner" : "",
-		 "bScrollCollapse" : true,
-		 "bPaginate" : true,
-		 "bJQueryUI" : true,
-		 });
-		 new FixedColumns(oTable);
-		 */
 		var oTable = $('#patient_listing').dataTable({
 			"sScrollX" : "100%",
 			"sScrollY" : "300px",
 			"bScrollCollapse" : true,
 			"bPaginate" : false,
 			"bJQueryUI" : true,
-			"bAutoWidth": false,
+			"bAutoWidth" : false,
 			"aoColumnDefs" : [{
 				"sWidth" : "10%",
 				"aTargets" : [-1]
