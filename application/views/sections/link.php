e144b43789a358a09a9b78474f1ad80205b1d187
KevinMarete55443 <kevomarete@gmail.com>
2014-01-31 19:39:26 +0300

    orders

--- application/views/sections/link.php
+++ application/views/sections/link.php
@@ -1,2 +1,2 @@
-<script type="text/javascript" src="http://localhost/NASCOP/assets/cache/1390473981b8f1d40c05735fe8c534f860e905a27f.js" charset="UTF-8"></script>
-<link type="text/css" rel="stylesheet" href="http://localhost/NASCOP/assets/cache/13909992417b0bbd27a4acf15a38e2865ca72cbe4f.css" media="screen" />
+<script type="text/javascript" src="http://localhost/NASCOP/assets/cache/1390227859b8f1d40c05735fe8c534f860e905a27f.js" charset="UTF-8"></script>
+<link type="text/css" rel="stylesheet" href="http://localhost/NASCOP/assets/cache/13911859687b0bbd27a4acf15a38e2865ca72cbe4f.css" media="screen" />
