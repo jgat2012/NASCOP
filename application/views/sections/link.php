3e18969f8259bfcc950605b3a2bc6cac8abf062d
KevinMarete55443 <kevomarete@gmail.com>
2014-02-09 15:36:34 +0300

    database

--- application/views/sections/link.php
+++ application/views/sections/link.php
@@ -1,2 +1,2 @@
-<script type="text/javascript" src="http://localhost/NASCOP/assets/cache/1391897192b8f1d40c05735fe8c534f860e905a27f.js" charset="UTF-8"></script>
-<link type="text/css" rel="stylesheet" href="http://localhost/NASCOP/assets/cache/139189730949f25aa95fd49fa6ddbc7412f58cee6b.css" media="screen" />
+<script type="text/javascript" src="http://localhost/NASCOP/assets/cache/1391758324b8f1d40c05735fe8c534f860e905a27f.js" charset="UTF-8"></script>
+<link type="text/css" rel="stylesheet" href="http://localhost/NASCOP/assets/cache/139194919949f25aa95fd49fa6ddbc7412f58cee6b.css" media="screen" />
