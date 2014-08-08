02a0640e745fbae1ba1cb83495aa8bc128216b14
KevinMarete <kevomarete@gmail.com>
2014-08-08 16:47:42 +0300

    changes

--- application/views/sections/link.php
+++ application/views/sections/link.php
@@ -1,2 +1,2 @@
-<script type="text/javascript" src="http://localhost/NASCOP/assets/cache/1402910767e9a47f29389af7f8dc4e6914c9069f53.js" charset="UTF-8"></script>
-<link type="text/css" rel="stylesheet" href="http://localhost/NASCOP/assets/cache/140291076738652f7f20d5676664079c2835f3d210.css" media="screen" />
+<script type="text/javascript" src="http://localhost/NASCOP/assets/cache/1401370006e9a47f29389af7f8dc4e6914c9069f53.js" charset="UTF-8"></script>
+<link type="text/css" rel="stylesheet" href="http://localhost/NASCOP/assets/cache/140137000638652f7f20d5676664079c2835f3d210.css" media="screen" />
